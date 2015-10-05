361f4543fb0ab14e345646f7c74c3ce9d2a799d5
Le Tran <LTran.1231@gmail.com>
2015-10-05 12:50:49 -0700

    helloworld app

---
+++ helloworld.java
@@ -0,0 +1,8 @@
+class HelloWorld {
+	public static void main(String args[]) {
+		System.out.println("Hello World!");
+	}
+}
+// everything begins with a class, you can't do anything without a class
+// inside the class is a list of methods
+// when Java run, it will run the main method
