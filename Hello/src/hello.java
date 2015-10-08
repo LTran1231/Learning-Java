7af5bf19d439510990658553150a13d3aa0fac14
Le Tran <LTran.1231@gmail.com>
2015-10-08 11:18:55 -0700

    Java Hello World app using Netbeans

---
+++ Hello/src/hello.java
@@ -0,0 +1,38 @@
+class hello 
+{
+    public static void main(String[] args) 
+    {
+        String word = "Hey this is my first varible!";
+        System.out.println(word);
+        
+        char letter = 'T';
+        System.out.println(letter);
+        
+        int number = 13;
+        System.out.println("My age is " + number);
+        
+        float decimal = 74.1256789f;
+        System.out.println("The temperature of today is " + decimal);
+        
+        boolean result = true;
+        System.out.println("The result is " + result);
+        
+        byte g = 127;
+        System.out.println(g);
+        
+        short numb = 30000;
+        System.out.println(numb);
+        
+        long lng = 24000102;
+        System.out.println(lng);
+        
+        double decimallong = 4.23453634932492423;
+        System.out.println(decimallong);
+        
+        System.out.print("My name is Le");
+        
+//      byte = integer number from -128 to +127
+//      short = integer number -32,768 to 32....
+//      long = 
+    }
+}
